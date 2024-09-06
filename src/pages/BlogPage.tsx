678738bf415d8e768b9065e8252657368e352e33
fanhe <hee.fan@outlook.com>
2024-09-06 23:33:13 +0800

    feat:add MDX support

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { MDXContent } from "@/components/MDXcontent";
+import Example from "@/content/Example.mdx";
 
 const BlogPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-white rounded-lg p-11 flex flex-row justify-center">
       <div>
         THERE WILL BE A LOT OF ARTICLES HERE
+        <MDXContent Content={Example} />
       </div>
     </div>
   );
